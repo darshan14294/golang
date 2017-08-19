0df777f1d64bcf44191f8e0a409a494697f45b1e
darshan14294 <darshan14294@gmail.com>
2017-08-19 17:33:22 +0530

    some simple exercise

---
+++ 02_package/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/darshan14294/golang/02_package/stringutil"
+	"github.com/darshan14294/golang/02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
